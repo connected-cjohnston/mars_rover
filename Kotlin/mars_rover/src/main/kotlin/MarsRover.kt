e6aeaae8e838758709bf6c226a18c477fb37822f
Chris Johnston <cjohnston@connected.io>
2023-04-03 14:18:13 -0400

    initial Kotlin commit

--- Kotlin/mars_rover/src/main/kotlin/MarsRover.kt
+++ Kotlin/mars_rover/src/main/kotlin/MarsRover.kt
@@ -1,2 +1,32 @@
-class MarsRover {
+class MarsRover(
+    private var x: Int, private var y: Int, private var orientation: String
+) {
+
+    private val cardinals = arrayOf("N", "E", "S", "W")
+    private var index: Int = 0
+
+    init {
+        index = cardinals.indexOf(orientation)
+    }
+
+    fun move() {
+        when (facing()) {
+            "N" -> y += 1
+            "E" -> x += 1
+            "S" -> y -= 1
+            "W" -> x -= 1
+        }
+    }
+
+    fun turnRight() {
+        index += 1
+    }
+
+    fun turnLeft() {
+        index -= 1
+    }
+
+    fun position(): String = "$x $y ${facing()}"
+
+    private fun facing(): String = cardinals[index.mod(4)]
 }
--- Kotlin/mars_rover/src/test/kotlin/MarsRoverTest.kt
+++ Kotlin/mars_rover/src/test/kotlin/MarsRoverTest.kt
@@ -1,9 +1,52 @@
-import org.junit.jupiter.api.Assertions.*
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class MarsRoverTest {
     @Test
-    fun testItWorks() {
-        assertTrue(true)
+    fun testReturnItsStartingPosition() {
+        val rover = MarsRover(2, 3, "N")
+        assertEquals("2 3 N", rover.position())
+    }
+
+    @Test
+    fun testRoverShouldMoveNorthOneSquare() {
+        val rover = MarsRover(2, 2, "N")
+        rover.move()
+        assertEquals("2 3 N", rover.position())
+    }
+
+    @Test
+    fun testRoverShouldMoveEastOneSquare() {
+        val rover = MarsRover(2, 2, "E")
+        rover.move()
+        assertEquals("3 2 E", rover.position())
+    }
+
+    @Test
+    fun testRoverShouldMoveSouthOneSquare() {
+        val rover = MarsRover(2, 2, "S")
+        rover.move()
+        assertEquals("2 1 S", rover.position())
+    }
+
+    @Test
+    fun testRoverShouldMoveWestOneSquare() {
+        val rover = MarsRover(2, 2, "W")
+        rover.move()
+        assertEquals("1 2 W", rover.position())
+    }
+
+    @Test
+    fun testRoverTurnsRight() {
+        val rover = MarsRover(2,2, "N")
+        rover.turnRight()
+        assertEquals("2 2 E", rover.position())
+    }
+
+    @Test
+    fun roverCanTurnLeft() {
+        val rover = MarsRover(2, 2, "N")
+        rover.turnLeft()
+        assertEquals("2 2 W", rover.position())
     }
 }
